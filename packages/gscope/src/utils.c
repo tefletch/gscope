7378c2efd989c9f491a9d5d98fcc5a663fe1650c
Tom Fletcher <tefletch@users.noreply.github.com>
2017-05-15 12:48:22 -0700

    Check for asprintf() with configure and utilize a basic substitute if needed

--- packages/gscope/src/utils.c
+++ packages/gscope/src/utils.c
@@ -340,3 +340,50 @@ void my_asprintf(gchar **str_ptr, const char *fmt, ...)
     }
     va_end(args);
 }
+
+
+#ifndef HAVE_ASPRINTF   // A glimmer of hope for those without asprintf() and friends.
+
+//=====================================================
+// WARNING:  This code has not been exhaustively tested
+//=====================================================
+
+#include <stdarg.h>
+int vasprintf(char **ret, const char *format, va_list ap)
+{
+    va_list ap2;
+    int len = 100;        /* First guess at the size */
+    if ((*ret = (char *)malloc(len)) == NULL)
+        return -1;
+    while (1)
+    {
+        int nchar;
+        va_copy(ap2, ap);
+        nchar = vsnprintf(*ret, len, format, ap2);
+        if (nchar > -1 && nchar < len)
+            return nchar;
+        if (nchar > len)
+            len = nchar + 1;
+        else
+            len *= 2;
+        if ((*ret = (char *)realloc(*ret, len)) == NULL)
+        {
+            free(*ret);
+            return -1;
+        }
+    }
+}
+
+#if 0   // my_asprintf() is the preferred calling method for Gscope
+int asprintf(char **ret, const char *format, ...)
+{
+    va_list ap;
+    int nc;
+    va_start(ap, format);
+    nc = vasprintf(ret, format, ap);
+    va_end(ap);
+    return nc;
+}
+#endif
+
+#endif
